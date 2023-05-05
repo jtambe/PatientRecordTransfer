df61df19047860bb371568525b8a83746803b897
Jayesh Tambe <tambe_jayesh@hotmail.com>
2023-05-04 22:18:52 -0700

    Success pop up at the end of file transfer demo

--- app/components/SendRecordsButton.js
+++ app/components/SendRecordsButton.js
@@ -1,12 +1,22 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import ConfirmDialog from './ConfirmDialog';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
 
 export default function SendRecordsButton({ provider, currentPatient, facility }) {
   const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
   const handleDialogClose = () => {
     setDialogOpen(false);
+    setOpen(true);
   };
 
   const handleDialogConfirm = (consentFile) => {
@@ -17,12 +27,25 @@ export default function SendRecordsButton({ provider, currentPatient, facility }
     setDialogOpen(true);
   };
 
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(false);
+  };
+
   return (
     <div>
       <Button variant="contained" color="primary" onClick={handleButtonClick} disabled={currentPatient.medicalRecords? false: true}>
         Send Records
       </Button>
       <ConfirmDialog currentPatient={currentPatient} provider={provider} facility={facility} open={dialogOpen} handleClose={handleDialogClose} handleConfirm={handleDialogConfirm} />
+      <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+        <Alert onClose={handleClose} severity="success" sx={{ width: '100%' }}>
+          Patient Files were transferred
+        </Alert>
+      </Snackbar>
     </div>
   );
 }
